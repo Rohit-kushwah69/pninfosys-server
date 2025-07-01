b5cb83f879fb9128b1a53daaa8882caa8d1cb3f4
Rohit3kushwah <rk6109744@gmail.com>
2025-07-01 00:51:17 -0500

    edit

--- database/connectDB.js
+++ database/connectDB.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const local_url = "mongodb://127.0.0.1:27017/api2";
+// const local_url = "mongodb://127.0.0.1:27017/api2";
 const live_url = "mongodb+srv://rohitkushwah6109744:ram123@cluster0.uh5u4.mongodb.net/Pninfosys-company?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectDB = async () => {
